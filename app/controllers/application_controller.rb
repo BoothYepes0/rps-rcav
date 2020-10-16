f8f252ec84efb5fd54207d0ae9c160ba62670878
yepes0 <yepes0@chicagobooth.edu>
2020-10-16 18:13:19 +0000

    rcav_yepes_v1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,11 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
+def play_rock
+  # write a ton of code
+  #redirect_to("https://wikipedia.org")
+  #render({ :plain => "Howdy, world!"})
+  #render({ :html => "<h1>Hello, world!</h1>".html_safe })
+  render({ :template => "game_templates/user_rock.html.erb" })
+end
 end
